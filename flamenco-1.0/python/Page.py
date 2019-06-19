b9fa41835cb5d90a56111cd309be562bfdf4847c
ankitadhandha <34309460+ankitadhandha@users.noreply.github.com>
2019-06-19 19:04:55 +0530

    Added code to add html link attribute to add favicons.

--- flamenco-1.0/python/Page.py
+++ flamenco-1.0/python/Page.py
@@ -152,6 +152,19 @@ Error message: <code>%s</code>
     def header(self, out):
         print >>out, '''<!doctype html public "-//W3C//DTD HTML 3.2 Final//EN">
 <html><head><link rel=stylesheet type="text/css" href="FlamencoStyle">
+<link rel="apple-touch-icon" sizes="57x57" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://dockerresourcesforwedge.s3.amazonaws.com/favicons/favicon-16x16.png">
 <meta %s><title>%s</title></head><body onload="%s">%s
 ''' % (self.metatag(), self.title(), self.preload(), bodyprefix)
         out.write(self.pagetop())
